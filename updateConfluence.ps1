9d634e260c234439702d44be39cb958427d5db04
karanotts <45759232+karanotts@users.noreply.github.com>
2019-02-28 16:03:37 +0000

    Update updateConfluence.ps1

--- updateConfluence.ps1
+++ updateConfluence.ps1
@@ -34,27 +34,6 @@ try {
     }
 }
 
-
-$connectionName = "AzureRunAsConnection"
-try {
-    # Get the connection "AzureRunAsConnection"
-    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
-
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-} catch {
-    if (!$servicePrincipalConnection) {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else {
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
-}
-
 $resourceGroups = Get-AzureRmResourceGroup
 $tableItems = @()
 if ($resourceGroups) 
