eb144d63d3a7d4fddafbcb1785a12c8c071a4a92
karanotts <45759232+karanotts@users.noreply.github.com>
2019-03-12 16:09:02 +0000

    Update azkeyvaultcert.ps1

--- azkeyvaultcert.ps1
+++ azkeyvaultcert.ps1
@@ -1,3 +1,4 @@
+# JIRA / Confluence vars
 $URI = "http:// #### "
 $PAGEID = " #### "
 $PAGEKEY = " #### "
@@ -6,7 +7,7 @@ $VER = "$URI/rest/api/content/$PAGEID"+"?expand=version"
 $UPDATE = "$URI/rest/api/content/$PAGEID"
 $ProjectKey = " #### "
 
-# generate authorisation header
+# Authorisation header for Confluence Invoke-RestMethod
 $user = " #### "
 $pass = " #### "
 $creds = "${user}:${pass}"
@@ -14,6 +15,12 @@ $bytes = [System.Text.Encoding]::UTF8.GetBytes($creds)
 $base64 = [System.Convert]::ToBase64String($bytes)
 $basicAuthValue = "Basic $base64"
 $header = @{ Authorization = $basicAuthValue }
+
+
+
+################################
+# ESTABLISH RUNBOOK CONNECTION #
+################################
 
 $connectionName = "AzureRunAsConnection"
 try {
@@ -35,6 +42,9 @@ try {
     }
 }
 
+###########################################################
+# GET INFORMATION ABOUT CERTIFICATED STORED IN KEY VAULTS #
+###########################################################
 
 $sub = ""
 Select-AzureRmSubscription $sub
@@ -97,45 +107,87 @@ $certs = (Get-AzureKeyVaultCertificate -VaultName $vault)
 
         $certItem = "<tr><td>$certName</td><td>$certVault</td><td>$certSubject</td><td>$certSubjectNam</td><td>$certDnsNames</td><td>$certKeyUsage</td><td>$certCreated</td><td>$certExpires</td></tr>"
         $tableItems += $certItem
-    }
+        
+        }
     }
 }
+
+
+##############################################
+# GENERATE SIMPLE HTML OUTPUT FOR CONFLUENCE #
+##############################################
 
 [string]$tableHeader = "<table cellpadding='15' cellspacing='2'><tr><th bgcolor='#f2f2f2'>Subscription</th><th bgcolor='#f2f2f2'>Name</th><th bgcolor='#f2f2f2'>Resource Group</th><th bgcolor='#f2f2f2'>Thumbprint</th><th bgcolor='#f2f2f2'>Created on</th><th bgcolor='#f2f2f2'>Expires on</th></tr>"
 [string]$tableFooter = "</table>"
 [string]$table = $tableHeader + $tableItems + $tableFooter
 
+# get page version (required by Confluence)
+$obj = Invoke-RestMethod -Uri $VER -Headers $header -Method GET | Select-Object -ExpandProperty version | Select-Object number
+$currentVersion = $obj.number
+$incVersion = [int]$currentVersion +1
+
+# Automation notification
+$warn = "<h4><span style='color:red'>WARNING!</span> This page is generated automatically, any manual changes will get overwritten!</h4>"
+
+# Generate Confluence page content
+$pageContent = "$warn $table"
+$pageContent
+$body = @{
+    id = $PAGEID
+    type = "page"
+    title = $PAGETITLE
+    space = @{ 
+        key = $PAGEKEY
+        }
+    body = @{
+        storage = @{
+            value = $pageContent
+            representation = "storage"
+            }
+        }
+    version = @{ 
+        number = $incVersion
+        }
+} | ConvertTo-Json -Depth 100
+
+try { 
+    Invoke-RestMethod -Uri " ### insert-uri ### " -Headers $header -Method PUT -ContentType "application/json" -Body $body 
+} 
+catch { Write-Output $_.Exception  }
 
 
+##################################################
+# CHECK IF SERVICE PRINCIPAL ACCESSED ALL VAULTS #
+##################################################
 
-#     # get page version
-#     $obj = Invoke-RestMethod -Uri $VER -Headers $header -Method GET | Select-Object -ExpandProperty version | Select-Object number
-#     $currentVersion = $obj.number
-#     $incVersion = [int]$currentVersion +1
+$vaults = (Get-AzureRmKeyVault).VaultName 
+foreach ($vault in $vaults) { 
+    try { 
+    $allCerts = Get-AzureKeyVaultCertificate -VaultName $vault -ErrorAction Stop
+    } 
+    catch [Microsoft.Azure.KeyVault.Models.KeyVaultErrorException] { 
+        $time=Get-Date 
+        Write-Output "$time $SPName couldn't read $vault : $_"
+        $subject = "Runbook couldn't read $vault"
+        $description  = "Service Principal $SPName could not access Key Vault $vault due to $_ and was unable to update JIRA & Confluence with the certificate metadata. Please address the issue immediately."
+    
+        # create a session in JIRA in order to stay logged in and make changes
+        Invoke-RestMethod -uri "$URI/rest/auth/latest/session" -ContentType "application/json" -Method POST -body $auth -SessionVariable mysession
 
-#     $warn = "<h4><span style='color:red'>WARNING!</span> This page is generated automatically, any manual changes will get overwritten!</h4>"
-
-#     $pageContent = "$warn $table"
-#     $pageContent
-#     $body = @{
-#         id = $PAGEID
-#         type = "page"
-#         title = $PAGETITLE
-#         space = @{ 
-#             key = $PAGEKEY
-#             }
-#         body = @{
-#             storage = @{
-#                 value = $pageContent
-#                 representation = "storage"
-#                 }
-#             }
-#         version = @{ 
-#             number = $incVersion
-#             }
-#     } | ConvertTo-Json -Depth 100
-
-#     try { 
-#         Invoke-RestMethod -Uri " ### insert-uri ### " -Headers $header -Method PUT -ContentType "application/json" -Body $body 
-#     } 
-#     catch { Write-Output $_.Exception  }
+        $global:IssueCreationBody = 
+            @{
+            fields = @{
+            project = @{ key = $projectKey }
+            summary = $subject
+            description = $description 
+            issuetype = @{ id = '10003'  }
+            # assignee = @{ name = $username }
+            priority = @{ id = '1'  }
+            }
+        } | ConvertTo-Json -Depth 100
+        
+        $IssueCreation = Invoke-WebRequest -uri "$URI/rest/api/latest/issue/" `
+        -ContentType "application/json" -Method POST -Body $IssueCreationBody -WebSession $mysession -UseBasicParsing | ConvertFrom-Json
+        $issueNumber = $IssueCreation.id
+    }
+} 
